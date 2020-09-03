3d4575c0cb475b0f1135df5f1d27b16debd14281
derpyasianpanda <minecraftsurvivor231@gmail.com>
2020-09-03 01:01:08 -0700

    🎀 Make AwesomeWM rc.lua prettier

--- dotfiles/awesome/rc.lua
+++ dotfiles/awesome/rc.lua
@@ -1,19 +1,68 @@
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
-pcall(require, "luarocks.loader")
+--           _____                    _____          
+--          /\    \                  /\    \         
+--         /::\    \                /::\    \        
+--        /::::\    \              /::::\    \       
+--       /::::::\    \            /::::::\    \      
+--      /:::/\:::\    \          /:::/\:::\    \     
+--     /:::/__\:::\    \        /:::/  \:::\    \    
+--    /::::\   \:::\    \      /:::/    \:::\    \   
+--   /::::::\   \:::\    \    /:::/    / \:::\    \  
+--  /:::/\:::\   \:::\____\  /:::/    /   \:::\    \ 
+-- /:::/  \:::\   \:::|    |/:::/____/     \:::\____\
+-- \::/   |::::\  /:::|____|\:::\    \      \::/    /
+--  \/____|:::::\/:::/    /  \:::\    \      \/____/ 
+--        |:::::::::/    /    \:::\    \             
+--        |::|\::::/    /      \:::\    \            
+--        |::| \::/____/        \:::\    \           
+--        |::|  ~|               \:::\    \          
+--        |::|   |                \:::\    \         
+--        \::|   |                 \:::\____\        
+--         \:|   |                  \::/    /        
+--          \|___|                   \/____/         
+--
+--           _____            _____                    _____          
+--          /\    \          /\    \                  /\    \         
+--         /::\____\        /::\____\                /::\    \        
+--        /:::/    /       /:::/    /               /::::\    \       
+--       /:::/    /       /:::/    /               /::::::\    \      
+--      /:::/    /       /:::/    /               /:::/\:::\    \     
+--     /:::/    /       /:::/    /               /:::/__\:::\    \    
+--    /:::/    /       /:::/    /               /::::\   \:::\    \   
+--   /:::/    /       /:::/    /      _____    /::::::\   \:::\    \  
+--  /:::/    /       /:::/____/      /\    \  /:::/\:::\   \:::\    \ 
+-- /:::/____/       |:::|    /      /::\____\/:::/  \:::\   \:::\____\
+-- \:::\    \       |:::|____\     /:::/    /\::/    \:::\  /:::/    /
+--  \:::\    \       \:::\    \   /:::/    /  \/____/ \:::\/:::/    / 
+--   \:::\    \       \:::\    \ /:::/    /            \::::::/    /  
+--    \:::\    \       \:::\    /:::/    /              \::::/    /   
+--     \:::\    \       \:::\__/:::/    /               /:::/    /    
+--      \:::\    \       \::::::::/    /               /:::/    /     
+--       \:::\    \       \::::::/    /               /:::/    /      
+--        \:::\____\       \::::/    /               /:::/    /       
+--         \::/    /        \::/____/                \::/    /        
+--          \/____/          ~~                       \/____/         
 
--- Standard awesome library
+-- ===================================================================
+-- Hello and welcome to derpyasianpanda's rc.lua for the "Awesome"
+-- window manager. I hope you enjoy your stay!
+-- ===================================================================
+
+-- ==========================
+-- Initializing Prerequisites
+-- ==========================
+
+-- Standard awesome libraries
 local gears = require("gears")
 local awful = require("awful")
 require("awful.autofocus")
 
--- Widget and layout library
+-- Widget/Layout library
 local wibox = require("wibox")
 
--- Theme handling library
+-- Theme library
 local beautiful = require("beautiful")
 
--- Notification library
+-- Notification libraries
 local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
@@ -22,7 +71,53 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
--- {{{ Error handling
+-- ============
+-- User options
+-- ============
+
+user = {
+    terminal = "kitty",
+    editor = os.getenv("EDITOR") or "vim",
+    browser = "google-chrome-stable",
+    theme = "static",
+    primary_key = "Mod1"
+}
+
+user["editor_cmd"] = user.terminal .. " " .. user.editor
+modkey = user.primary_key
+superkey = "Mod4"
+mod1 = user.primary_key
+mod2 = user.secondary_key
+
+-- Table of layouts
+awful.layout.layouts = {
+    awful.layout.suit.tile,
+    awful.layout.suit.floating,
+    awful.layout.suit.max,
+    awful.layout.suit.spiral.dwindle,
+    --awful.layout.suit.spiral,
+    --awful.layout.suit.tile.top,
+    --awful.layout.suit.fair,
+    --awful.layout.suit.fair.horizontal,
+    --awful.layout.suit.tile.left,
+    --awful.layout.suit.tile.bottom,
+    --awful.layout.suit.max.fullscreen,
+    --awful.layout.suit.corner.nw,
+    --awful.layout.suit.magnifier,
+    --awful.layout.suit.corner.ne,
+    --awful.layout.suit.corner.sw,
+    --awful.layout.suit.corner.se,
+}
+
+-- Theme Initialization
+beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/" ..
+    user.theme .. "/theme.lua")
+
+
+-- ==============
+-- Error handling
+-- ==============
+
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
@@ -45,100 +140,37 @@ do
         in_error = false
     end)
 end
--- }}}
 
--- User variables
-user = {
-    terminal = "kitty",
-    editor = os.getenv("EDITOR") or "vim",
-    browser = "google-chrome-stable",
-    theme = "static",
-    primary_key = "Mod1"
-}
+-- ==========
+-- Menu Setup
+-- ==========
 
-user["editor_cmd"] = user.terminal .. " " .. user.editor
-modkey = user.primary_key
-superkey = "Mod4"
-mod1 = user.primary_key
-mod2 = user.secondary_key
-
--- Theme Initialization
--- beautiful.init(gears.filesystem.get_themes_dir() .. "gtk/theme.lua")
-beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/" .. user.theme .. "/theme.lua")
-
--- Table of layouts
-awful.layout.layouts = {
-    awful.layout.suit.tile,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.floating,
-    awful.layout.suit.max
-}
-
--- {{{ Menu
 -- Create a launcher widget and a main menu
 myawesomemenu = {
-   { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
---   { "manual", user.terminal .. " -e man awesome" },
+   { "hotkeys", function() 
+       hotkeys_popup.show_help(nil, awful.screen.focused())
+   end },
+   { "manual", user.terminal .. " -e man awesome" },
    { "edit config", user.editor_cmd .. " " .. awesome.conffile },
    { "restart", awesome.restart },
    { "quit", function() awesome.quit() end },
 }
 
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "open terminal", user.terminal }
-                                  }
-                        })
+mymainmenu = awful.menu({ items = { 
+    { "awesome", myawesomemenu, beautiful.awesome_icon },
+    { "open terminal", user.terminal }
+}})
 
-mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-                                     menu = mymainmenu })
+mylauncher = awful.widget.launcher({
+    image = beautiful.awesome_icon,
+    menu = mymainmenu
+})
 
--- Menubar configuration
-menubar.utils.terminal = user.terminal -- Set the terminal for applications that require it
--- }}}
+menubar.utils.terminal = user.terminal
 
--- {{{ Wibar
-
--- Create a wibox for each screen and add it
-local mytextclock = wibox.widget.textclock(" ~ %a %b %e, %H:%M ~ ")
-
-local taglist_buttons = gears.table.join(
-                    awful.button({ }, 1, function(t) t:view_only() end),
-                    awful.button({ modkey }, 1, function(t)
-                                              if client.focus then
-                                                  client.focus:move_to_tag(t)
-                                              end
-                                          end),
-                    awful.button({ }, 3, awful.tag.viewtoggle),
-                    awful.button({ modkey }, 3, function(t)
-                                              if client.focus then
-                                                  client.focus:toggle_tag(t)
-                                              end
-                                          end),
-                    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
-                    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
-                )
-
-local tasklist_buttons = gears.table.join(
-                     awful.button({ }, 1, function (c)
-                                              if c == client.focus then
-                                                  c.minimized = true
-                                              else
-                                                  c:emit_signal(
-                                                      "request::activate",
-                                                      "tasklist",
-                                                      {raise = true}
-                                                  )
-                                              end
-                                          end),
-                     awful.button({ }, 3, function()
-                                              awful.menu.client_list({ theme = { width = 250 } })
-                                          end),
-                     awful.button({ }, 4, function ()
-                                              awful.client.focus.byidx(1)
-                                          end),
-                     awful.button({ }, 5, function ()
-                                              awful.client.focus.byidx(-1)
-                                          end))
+-- ===============
+-- Wallpaper Setup
+-- ===============
 
 local function set_wallpaper(s)
     -- Wallpaper
@@ -155,6 +187,74 @@ end
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
 screen.connect_signal("property::geometry", set_wallpaper)
 
+-- Dynamic Wallpaper Setting
+script_path = gears.filesystem.get_configuration_dir() .. "scripts/"
+if beautiful.dynamic_wallpaper then
+    bg_path = gears.filesystem.get_configuration_dir() .. "themes/" .. user.theme .. "/background.mp4"
+    awful.spawn.with_shell(script_path .. "setlivewallpaper.sh " .. bg_path)
+else
+    awful.spawn.with_shell(script_path .. "killlivewallpapers.sh")
+end
+
+-- ===========
+-- Wibar Setup
+-- ===========
+
+local mytextclock = wibox.widget.textclock(" ~ %a %b %e, %H:%M ~ ")
+
+local taglist_buttons = gears.table.join(
+    awful.button({ }, 1, function(t) t:view_only() end),
+    awful.button({ modkey }, 1,
+        function(t)
+            if client.focus then
+                client.focus:move_to_tag(t)
+            end
+        end),
+    awful.button({ }, 3, awful.tag.viewtoggle),
+    awful.button({ modkey }, 3,
+        function(t)
+            if client.focus then
+                client.focus:toggle_tag(t)
+            end
+        end),
+    awful.button({ }, 4,
+        function(t) awful.tag.viewnext(t.screen) end),
+    awful.button({ }, 5,
+        function(t) awful.tag.viewprev(t.screen) end)
+)
+
+local tasklist_buttons = gears.table.join(
+    awful.button({ }, 1,
+        function (c)
+            if c == client.focus then
+                c.minimized = true
+            else
+                c:emit_signal(
+                    "request::activate",
+                    "tasklist",
+                    { raise = true }
+                )
+            end
+        end),
+
+    awful.button({ }, 3,
+        function()
+            awful.menu.client_list({
+                theme = { width = 250 }
+            })
+        end),
+
+    awful.button({ }, 4,
+        function ()
+            awful.client.focus.byidx(1)
+        end),
+
+    awful.button({ }, 5,
+        function ()
+            awful.client.focus.byidx(-1)
+        end)
+)
+
 -- Extra custom widgets to use
 local spotify_widget = require("awesome-wm-widgets.spotify-widget.spotify")
 
@@ -164,6 +264,7 @@ local function padding_widget (pad_size)
     return wibox.widget.textbox(string.rep(" ", pad_size))
 end
 
+-- Add a wibar to each screen
 awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
@@ -186,7 +287,7 @@ awful.screen.connect_for_each_screen(function(s)
     }
 
     -- Serves as padding for the top widgets
-    s.mywibox = awful.wibar({ position = "top", screen = s, opacity = 0, height = 58 })
+    s.mywibox = awful.wibar({ position = "top", screen = s, opacity = 0, height = 50 })
     s.mywibox:setup();
 
     s.tag_wibox = wibox({
@@ -194,9 +295,9 @@ awful.screen.connect_for_each_screen(function(s)
         border_color = beautiful.bg_normal,
         ontop = false,
         x = s.geometry["x"] + 8 * 1.5,
-        y = s.geometry["y"] + 8,
+        y = s.geometry["y"] + 7,
         width = 112,
-        height = 30,
+        height = 25,
         screen = s,
         visible = true,
     })
@@ -214,9 +315,9 @@ awful.screen.connect_for_each_screen(function(s)
         border_color = beautiful.bg_normal,
         ontop = false,
         x = s.geometry["x"] + s.geometry["width"] / 2 - 125,
-        y = s.geometry["y"] + 8,
+        y = s.geometry["y"] + 7,
         width = 250,
-        height = 30,
+        height = 25,
         screen = s,
         visible = true,
     })
@@ -237,14 +338,14 @@ awful.screen.connect_for_each_screen(function(s)
         border_color = beautiful.bg_normal,
         ontop = false,
         width = 100,
-        height = 30,
+        height = 25,
         screen = s,
         visible = true,
     })
-    s.util_bar.y = s.geometry["y"] + 8
+    s.util_bar.y = s.geometry["y"] + 7
 
     if s.index == 1 then
-        s.util_bar.x = s.geometry["x"] + s.geometry["width"] - 100 - 8 * 1.5 * 2.5,
+        s.util_bar.x = s.geometry["x"] + s.geometry["width"] - 100 - 12 * 1.5 * 2,
         s.util_bar:setup {
             layout = wibox.layout.align.horizontal,
             {
@@ -254,7 +355,7 @@ awful.screen.connect_for_each_screen(function(s)
         }
     else
         s.util_bar.width = 300
-        s.util_bar.x = s.geometry["x"] + s.geometry["width"] - s.util_bar.width - 25
+        s.util_bar.x = s.geometry["x"] + s.geometry["width"] - s.util_bar.width - 35
         s.util_bar:setup {
             layout = wibox.layout.align.horizontal,
             {
@@ -272,15 +373,18 @@ awful.screen.connect_for_each_screen(function(s)
 end)
 -- }}}
 
--- {{{ Mouse bindings
+-- ==================
+-- Bindings/Shortcuts
+-- ==================
+
+-- Mouse
 root.buttons(gears.table.join(
     awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
 ))
--- }}}
 
--- {{{ Key bindings
+-- Global Keys
 globalkeys = gears.table.join(
     awful.key({ modkey }, "s", hotkeys_popup.show_help,
               { description="show help", group="awesome" }),
@@ -411,9 +515,7 @@ clientkeys = gears.table.join(
         { description = "(un)maximize horizontally", group = "client" })
 )
 
--- Bind all key numbers to tags.
--- Be careful: we use keycodes to make it work on any keyboard layout.
--- This should map on the top row of your keyboard, usually 1 to 9.
+-- Taglist Controls
 for i = 1, 9 do
     globalkeys = gears.table.join(globalkeys,
         -- View tag only.
@@ -480,12 +582,11 @@ clientbuttons = gears.table.join(
 
 -- Set keys
 root.keys(globalkeys)
--- }}}
 
 -- {{{ Rules
 -- Rules to apply to new clients (through the "manage" signal).
 awful.rules.rules = {
-    -- All clients will match this rule.
+    -- All clients
     { rule = { },
         properties = {
             border_width = beautiful.border_width,
@@ -501,39 +602,10 @@ awful.rules.rules = {
 
     -- Floating clients.
     { rule_any = {
-        instance = {
-          "DTA",  -- Firefox addon DownThemAll.
-          "copyq",  -- Includes session name in class.
-          "pinentry",
-        },
-        class = {
-          "Arandr",
-          "Blueman-manager",
-          "Gpick",
-          "Kruler",
-          "MessageWin",  -- kalarm.
-          "Sxiv",
-          "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-          "Wpa_gui",
-          "veromix",
-          "xtightvncviewer"},
-
-        -- Note that the name property shown in xprop might be set slightly after creation of the client
-        -- and the name shown there might not match defined rules here.
-        name = {
-          "Event Tester",  -- xev.
-        },
         role = {
-          "AlarmWindow",  -- Thunderbird's calendar.
-          "ConfigManager",  -- Thunderbird's about:config.
           "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
         }
       }, properties = { floating = true }},
-
-    -- Add titlebars to normal clients and dialogs
-    { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = false }
-    },
 }
 -- }}}
 
@@ -543,7 +615,6 @@ client.connect_signal("manage", function (c)
     -- Set the windows at the slave,
     -- i.e. put it at the end of others instead of setting it master.
     -- if not awesome.startup then awful.client.setslave(c) end
-
     if awesome.startup
       and not c.size_hints.user_position
       and not c.size_hints.program_position then
@@ -552,7 +623,7 @@ client.connect_signal("manage", function (c)
     end
 end)
 
--- Enable sloppy focus, so that focus follows mouse.
+-- Sloppy Focus
 client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
 end)
@@ -561,14 +632,8 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
--- Autostart
-awful.spawn.with_shell("picom -cb --experimental-backends")
+-- ===============
+-- Autostart Setup
+-- ===============
 
--- Dynamic Wallpaper Setting
-script_path = gears.filesystem.get_configuration_dir() .. "scripts/"
-if beautiful.dynamic_wallpaper then
-    bg_path = gears.filesystem.get_configuration_dir() .. "themes/" .. user.theme .. "/background.mp4"
-    awful.spawn.with_shell(script_path .. "setlivewallpaper.sh " .. bg_path)
-else
-    awful.spawn.with_shell(script_path .. "killlivewallpapers.sh")
-end
+awful.spawn.with_shell("picom -cb --experimental-backends")
