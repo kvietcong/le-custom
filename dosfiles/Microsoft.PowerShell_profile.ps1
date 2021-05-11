d704678400ae0e90cbc26c7be2a2061371a61984
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2021-05-10 22:46:37 -0700

    ✅ Add windows specific configuration files

---
+++ dosfiles/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+Set-Alias -Name vim -Value nvim
+Set-PoshPrompt -Theme ~/.mytheme.omp.json
+
+function DirCS { cd 'D:\Documents\Computer Science\' }
+function DirUW { cd 'D:\Documents\UW\' }
+function DirAppData { cd '~\AppData\' }
