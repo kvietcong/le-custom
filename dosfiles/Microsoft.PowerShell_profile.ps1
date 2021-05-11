1fb8ddb657300d0bcf545e89e3fd7b9118af8267
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2021-05-10 23:53:12 -0700

    📝 Add a new notes function for Powershell

--- dosfiles/Microsoft.PowerShell_profile.ps1
+++ dosfiles/Microsoft.PowerShell_profile.ps1
@@ -2,5 +2,6 @@ Set-Alias -Name vim -Value nvim
 Set-PoshPrompt -Theme ~/.mytheme.omp.json
 
 function DirCS { cd 'D:\Documents\Computer Science\' }
+function DirNotes { cd 'D:\Documents\Obsidian\' }
 function DirUW { cd 'D:\Documents\UW\' }
 function DirAppData { cd '~\AppData\' }
