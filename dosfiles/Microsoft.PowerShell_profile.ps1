5ad6b4ad0ecd07042b7e2c96248909cf18b88193
KV Le <32186663+kvietcong@users.noreply.github.com>
2021-05-12 19:34:05 -0700

    ➕ Add more Windows stuff

--- dosfiles/Microsoft.PowerShell_profile.ps1
+++ dosfiles/Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,7 @@ Set-Alias -Name vim -Value nvim
 Set-PoshPrompt -Theme ~/.mytheme.omp.json
 
 function DirCS { cd 'D:\Documents\Computer Science\' }
+function DirDots { cd 'D:\Documents\le-custom\' }
 function DirNotes { cd 'D:\Documents\Obsidian\' }
 function DirUW { cd 'D:\Documents\UW\' }
 function DirAppData { cd '~\AppData\' }
