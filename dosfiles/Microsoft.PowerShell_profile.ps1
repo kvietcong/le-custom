0ae9d0dfb3ff9432217bde1ab1245ce42b0becf2
KV Le <32186663+kvietcong@users.noreply.github.com>
2021-06-01 15:14:43 -0700

    📝 Edit note directory

--- dosfiles/Microsoft.PowerShell_profile.ps1
+++ dosfiles/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,6 @@ Set-PoshPrompt -Theme ~/.mytheme.omp.json
 
 function DirCS { cd 'D:\Documents\Computer Science\' }
 function DirDots { cd 'D:\Documents\le-custom\' }
-function DirNotes { cd 'D:\Documents\Obsidian\' }
+function DirNotes { cd 'D:\Documents\Notes\' }
 function DirUW { cd 'D:\Documents\UW\' }
 function DirAppData { cd '~\AppData\' }
