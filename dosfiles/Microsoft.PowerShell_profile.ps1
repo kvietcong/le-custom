01758da18d1666a4ef67474851d1ee2728c3fd28
KV Le <32186663+kvietcong@users.noreply.github.com>
2021-07-19 12:56:02 -0700

    ✅ Add new aliases and Update Theme

--- dosfiles/Microsoft.PowerShell_profile.ps1
+++ dosfiles/Microsoft.PowerShell_profile.ps1
@@ -7,4 +7,5 @@ function DirNotes { cd 'D:\Documents\Notes\' }
 function DirUW { cd 'D:\Documents\UW\' }
 function DirAppData { cd '~\AppData\' }
 
-function Notes { cd 'D:\Documents\Notes\' && nvim }
+function Notes { cd 'D:\Documents\Notes\' && nvim Index.md}
+function ConfigNvim { dirdots && cd 'dotfiles\.config\nvim' && nvim lua\\init.lua init.vim }
