ffa1412f3585fad09366d2c1963567d32ec9d593
KV Le <32186663+kvietcong@users.noreply.github.com>
2021-07-19 17:27:44 -0700

    ✅ Replace change directory with Zoxide

--- dosfiles/Microsoft.PowerShell_profile.ps1
+++ dosfiles/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 Set-Alias -Name vim -Value nvim
+Set-Alias -Name cd -Value z -Option AllScope
 Set-PoshPrompt -Theme ~/.mytheme.omp.json
 
 function DirCS { cd 'D:\Documents\Computer Science\' }
@@ -8,4 +9,9 @@ function DirUW { cd 'D:\Documents\UW\' }
 function DirAppData { cd '~\AppData\' }
 
 function Notes { cd 'D:\Documents\Notes\' && nvim Index.md}
-function ConfigNvim { dirdots && cd 'dotfiles\.config\nvim' && nvim lua\\init.lua init.vim }
+function ConfigNvim { dirdots && cd 'dotfiles\.config\nvim' && nvim }
+
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell) -join "`n"
+})
